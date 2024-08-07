ea9fcbf3192ff3c72278b32e5290786803ae3def
afariya <tilikiptiken@gmail.com>
2024-08-07 13:03:35 +0300

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
-
-// Check the URL for the resume PDF parameter
-const urlParams = new URLSearchParams(window.location.search);
-const pdfUrl = urlParams.get('pdf');
-
-// Function to highlight the active resume link
-function highlightActiveLink() {
+window.onload = function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const pdfUrl = urlParams.get('pdf');
     if (pdfUrl) {
-        // Remove active class from all links
-        document.querySelectorAll('.dropdown-item').forEach(item => {
-            item.classList.remove('active');
-        });
+        document.getElementById('pdfEmbed').src = pdfUrl;
+    } else {
+        document.body.innerHTML = '<p>No PDF file specified</p>';
+    }
 
-        // Add active class to the corresponding link
-        if (pdfUrl.includes('resume-english.pdf')) {
-            document.getElementById('resumeEnglish').classList.add('active');
-        } else if (pdfUrl.includes('resume-kiswahili.pdf')) {
-            document.getElementById('resumeKiswahili').classList.add('active');
-        } else if (pdfUrl.includes('resume-indigenous.pdf')) {
-            document.getElementById('resumeIndigenous').classList.add('active');
+    // Function to highlight the active resume link
+    function highlightActiveLink() {
+        if (pdfUrl) {
+
+            document.querySelectorAll('.navbar-nav .nav-link').forEach(item => {
+                item.classList.remove('active');
+            });
+
+            // Remove active class from all links
+            document.querySelectorAll('.dropdown-item').forEach(item => {
+                item.classList.remove('active');
+            });
+
+            // Add active class to the corresponding link
+            if (pdfUrl.includes('doc/clarrison RESUME.pdf')) {
+                document.getElementById('resumeEnglish').classList.add('active');
+                document.querySelector('.nav-link[href="resume.html"]').classList.add('active');
+            } else if (pdfUrl.includes('doc/kiwahili RESUME.pdf')) {
+                document.getElementById('resumeKiswahili').classList.add('active');
+                document.querySelector('.nav-link[href="resume.html"]').classList.add('active');
+            } else if (pdfUrl.includes('doc/Kalenjin.pdf')) {
+                document.getElementById('resumeIndigenous').classList.add('active');
+                document.querySelector('.nav-link[href="resume.html"]').classList.add('active');
+            }
         }
     }
-}
 
-// Call the function on page load
-window.onload = highlightActiveLink;
+    // Call the function on page load
+    highlightActiveLink();
+}
